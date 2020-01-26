e77d345720ff8177138ea13c0ea857e1996edad0
Ivo C.S. Wingelaar <ivo@codeforge.rs>
2020-01-26 16:06:57 +0100

    Add some extra exports to the root file

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,7 @@ pub mod error;
 pub mod verifier;
 
 pub use error::TResult;
-pub use verifier::{Sort, State, Stepper, Table, Table_, Theorem, Type};
+pub use verifier::{
+    Context, Sort, Sort_, State, Stepper, Store, Store_, Table, Table_, Term, Term_, Theorem,
+    Theorem_, Type, Type_,
+};
--- src/verifier/mod.rs
+++ src/verifier/mod.rs
@@ -76,6 +76,8 @@ impl Type for Type_ {
     }
 }
 
+pub use context::store::{Store, Store_};
+pub use context::Context;
 pub use state::State;
 pub use stream::Stepper;
 pub use table::{Sort, Sort_, Table, Table_, Term, Term_, Theorem, Theorem_};
